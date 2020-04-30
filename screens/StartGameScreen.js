64f79ff537d41e7072bdaee7f1567c9978cb1423
Kush <kush.bhatt96@yahoo.com>
2020-04-29 19:57:52 -0600

    Added a GameScreen component and code for generating a random guess

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -11,6 +11,7 @@ import {
 } from "react-native";
 import Card from "../components/Card";
 import Colors from "../constants/Colors";
+import NumberContainer from '../components/NumberContainer';
 import Input from "../components/Input";
 
 const StartGameScreen = (props) => {
@@ -30,22 +31,27 @@ const StartGameScreen = (props) => {
   const confirmInputHandler = () => {
     const chosenNumber = parseInt(enteredValue);
     if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
-      Alert.alert(
-        "Invalid number!",
-        "Number must be between 1 and 99.",
-        [{ text: "Okay", style: "destructive", onPress: resetInputHandler }]
-      );
+      Alert.alert("Invalid number!", "Number must be between 1 and 99.", [
+        { text: "Okay", style: "destructive", onPress: resetInputHandler },
+      ]);
       return; //essentially if the number is invalid, nothing will happen
     }
     setConfirmed(true);
     setSelectedNumber(chosenNumber);
     setEnteredValue("");
+    Keyboard.dismiss();
   };
 
   let confirmedOutput;
 
   if (confirmed) {
-    confirmedOutput = <Text>Chosen Number: {selectedNumber}</Text>;
+    confirmedOutput = (
+      <Card style={styles.summaryContainer}>
+        <Text>You selected</Text>
+        <NumberContainer>{selectedNumber}</NumberContainer>
+        <Button title="START GAME"/>
+      </Card>
+    );
   }
 
   return (
@@ -118,6 +124,10 @@ const styles = StyleSheet.create({
   input: {
     width: 50,
     textAlign: "center",
+  },
+  summaryContainer: {
+    marginTop: 20,
+    alignItems: 'center'
   },
 });
 
